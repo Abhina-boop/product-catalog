bec072da9dbff77771df16bf5561b6b8c33435ed
Abhina-boop <sundarabhina91@gmail.com>
2025-11-24 13:42:56 +0530

    Fix CORS origin issue

--- app/main.py
+++ app/main.py
@@ -4,24 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.routes import products
 
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Product Catalog API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://papaya-pika-064f41.netlify.app/"],  
+    allow_origins=["https://papaya-pika-064f41.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(products.router)
 
 @app.get("/")
 def root():
     return {"message": "Product Catalog API running"}
-
-
